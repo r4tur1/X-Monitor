2cf8cca3b34b443c42d94155724140af811746fe
r4tur1 <aadityaraturi11@gmail.com>
2025-07-02 15:50:54 +0000

    Delete web directory

--- web/script.js
+++
@@ -1,6 +0,0 @@
-async function fetchTweets() {
-  const keyword = document.getElementById("keyword").value;
-  const response = await fetch(`https://your-python-backend.herokuapp.com/search?q=${keyword}`);
-  const tweets = await response.json();
-  document.getElementById("results").innerHTML = JSON.stringify(tweets);
-}
